5717a205783005500ac1dfc2d2325799800d966e
Deniz Evrendilek <devrendilek@gmail.com>
2023-08-03 09:58:39 -0700

    register student info

--- src/tokenClasses.cpp
+++ src/tokenClasses.cpp
@@ -1,34 +1,29 @@
 //============================================================================
 //
-//% Student Name 1: student1
-//% Student 1 #: 123456781
-//% Student 1 userid (email): stu1 (stu1@sfu.ca)
-//
-//% Student Name 2: student2
-//% Student 2 #: 123456782
-//% Student 2 userid (email): stu2 (stu2@sfu.ca)
+//% Student Name 1: Deniz Eren Evrendilek
+//% Student 1 #: 301340591
+//% Student 1 userid (email): devrendi (devrendi@sfu.ca)
 //
 //% Below, edit to list any people who helped you with the code in this file,
-//%      or put NONE if nobody helped (the two of) you.
+//%      or put 'None' if nobody helped (the two of) you.
 //
-// Helpers: _everybody helped us/me with the assignment (list names or put
-// �none�)__
+// Helpers: None
 //
 // Also, list any resources beyond the course textbook and the course pages on
 // Piazza that you used in making your submission.
 //
-// Resources:  ___________
+// Resources:  None
 //
 //%% Instructions:
 //% * Put your name(s), student number(s), userid(s) in the above section.
-//% * Enter the above information in tokenClasses.hpp  too.
+//% * Enter the above information in tokenClasses.cpp  too.
 //% * Edit the "Helpers" line and "Resources" line.
-//% * Your group name should be "P4_<userid1>_<userid2>" (eg. P4_stu1_stu2)
+//% * Your group name should be "P2_<userid1>_<userid2>" (eg. P2_stu1_stu2)
 //% * Form groups as described at:  https://courses.cs.sfu.ca/docs/students
 //% * Submit files to courses.cs.sfu.ca
 //
-// Name        : tokenClasses.cpp
-// Description : Course Project Part 4
+// Name        : tokenClassifier.cpp
+// Description : Course Project Part 2 (Token Classifier)
 // Original portions Copyright (c) 2023 School of Engineering Science, Simon
 // Fraser University
 //============================================================================
